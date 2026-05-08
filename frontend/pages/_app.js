bfa988d67f3da96fc1da37f976f01a6545f0602d
luzzy97 <priansyahjuli@gmail.com>
2026-05-09 03:46:36 +0700

    Update title to UniFAIR | Unified DeFi and change favicon 🚀

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -26,11 +26,9 @@ export default function App({ Component, pageProps }) {
           <GlobalToast />
           <ActiveTasks />
           <Head>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-            <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet" />
-            <title>UniFAIR | Intelligent DeFi Hub</title>
+            <title>UniFAIR | Unified DeFi</title>
+            <meta name="description" content="Intelligent DeFi Hub" />
+            <link rel="icon" href="/favicon.ico" />
           </Head>
           <Component {...pageProps} />
         </WalletProvider>
